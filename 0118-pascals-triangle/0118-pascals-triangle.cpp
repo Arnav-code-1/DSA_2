0ab364126dc921f5374370217503ff3b0776117b
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-06-20 11:10:42 +0530

    Time: 6 ms (8.07%), Space: 6.8 MB (38.87%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> r(numRows);
-        for(int i=0;i<numRows;i++)
-        {
-            r[i].resize(i+1);
-            r[i][0]=r[i][i]=1;
-            for(int j=1;j<i;j++)
-            r[i][j]=r[i-1][j-1]+r[i-1][j];
 
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r;
+        for(int i=1;i<=numRows;i++)
+        {
+            vector<int> res;
+            res.push_back(1);
+            int ans=1;
+          for(int j=1;j<i;j++)
+        {
+        ans=ans*(i-j);
+        ans=ans/j;
+        res.push_back(ans);
+        }
+        r.push_back(res);
         }
         return r;
     }
