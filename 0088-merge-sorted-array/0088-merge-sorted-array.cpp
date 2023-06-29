5bd84ad2745e41fd661730a1c89bf758930ca407
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-06-29 14:31:52 +0530

    Time: 0 ms (100%), Space: 9.1 MB (15.3%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        int insertIndex = m + n - 1; // Index to insert elements in nums1
+
+        int i = m - 1; // Index for elements in nums1
+        int j = n - 1; // Index for elements in nums2
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[insertIndex--] = nums1[i--];
+            } else {
+                nums1[insertIndex--] = nums2[j--];
+            }
+        }
+
+        // If there are remaining elements in nums2, insert them into nums1
+        while (j >= 0) {
+            nums1[insertIndex--] = nums2[j--];
+        }
+    }
+};
