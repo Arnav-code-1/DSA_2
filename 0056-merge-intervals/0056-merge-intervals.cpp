8c514e22b6037fc4a79afde11906513467d56385
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-06-29 12:37:06 +0530

    Time: 34 ms (95.13%), Space: 18.9 MB (94.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,8 +7,10 @@ public:
        for(int i=0;i<n;i++)
        {
            if(ans.empty() || intervals[i][0]>ans.back()[1])
+           {
            ans.push_back(intervals[i]);
-           else
+           continue;
+           }
            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
        }
        return ans;
