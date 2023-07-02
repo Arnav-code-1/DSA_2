185167c6a1db46de670d931a59e91e0be157cb6c
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-07-02 19:20:31 +0530

    Time: 3 ms (84.87%), Space: 9.5 MB (60.92%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
 //Binary Search used
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-         int row=matrix.size();
-         for(int i=0;i<row;i++)
-         {
-             int s=0;
-             int e=matrix[i].size()-1;
-             int mid=s+(e-s)/2;
-             while(s<=e)
-             {
-                 if(matrix[i][mid]==target)
-                 {
-                     return true;
-                 }
-                 if(matrix[i][mid]>target)
-                 {
-                     e=mid-1;
-                 }
-                 else
-                 {
-                     s=mid+1;
-                 }
-                 mid=s+(e-s)/2;
-             }
-         }
-         return false;
+        int rows=matrix.size();
+        int col=matrix[0].size();
+        int total=rows*col;
+        int s=0;
+        int e=total-1;
+        int mid=s+(e-s)/2;
+        while(s<=e)
+        {
+            int elementrow=mid/col;
+            int elementcol=mid%col;
+            if(matrix[elementrow][elementcol]==target)
+            return true;
+            if(matrix[elementrow][elementcol]>target)
+            {
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return false;
     }
 };
