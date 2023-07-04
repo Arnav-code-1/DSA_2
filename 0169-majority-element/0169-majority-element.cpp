ada1d19e943e1ea4b5bd9352929ed522f043255b
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-07-04 13:35:53 +0530

    Time: 30 ms (13.97%), Space: 19.5 MB (94.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> Map;
+        int cnt=0;
+        int element;
         for(int i=0;i<nums.size();i++)
         {
-            Map[nums[i]]++;
+            if(cnt==0)
+            {
+                element =nums[i];
+            }
+            if(element==nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
         }
-        for(auto it:Map)
+        //above we found the element who has the highest frequency
+        for(int i=0;i<nums.size();i++)
         {
-            if(it.second>(nums.size()/2))
-            return it.first;
+            if(element==nums[i])
+            cnt++;
+            if(cnt>(nums.size()/2))
+            return element;
         }
         return -1;
     }
