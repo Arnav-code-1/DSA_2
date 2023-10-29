709c0acbfb5905a8ec0e74b89475586078f4cac6
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-10-29 19:00:35 +0530

    Time: 4 ms (31.16%), Space: 9.2 MB (26.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int> ans;
-        int count =0;
-        for(int i=0 ;i<nums.size();i++){
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
-               ans.push_back(nums[i]);
-            }
+                nums[++index]=nums[i];
+            } 
         }
-        nums= ans;
-        return nums.size();
+        return index+1;
     }
 };
