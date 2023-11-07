e128a5ca8595dc6ebb4ad213d4ec5dd537115a45
Arnav-code-1 <85742154+Arnav-code-1@users.noreply.github.com>
2023-11-07 23:32:12 +0530

    Time: 5 ms (43.21%), Space: 7.6 MB (52.73%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node* temp=head;
+        while(temp){
+            if(temp->child){
+                Node* fake=temp->child;
+                while(fake->next){
+                    fake=fake->next;
+                }
+                fake->next=temp->next;
+                if(temp->next){
+                    temp->next->prev=fake;
+                }
+                temp->child->prev=temp;
+                temp->next=temp->child;
+                temp->child=NULL;
+            }
+            else{
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
